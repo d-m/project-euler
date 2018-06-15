08801bd272bd216b6eccba5bad848498c98172c1
Derek Morgan <2354832+d-m@users.noreply.github.com>
2018-06-15 00:33:09 -0400

    one more problem

--- 021.py
+++ 021.py
@@ -13,5 +13,41 @@ For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22,
 
 Evaluate the sum of all the amicable numbers under 10000.
 """
+import collections
+import math
+
+
+def euler():
+    sum_of_amicable_numbers = 0
+    proper_divisor_sums = {}
+    for x in range(2, 10000):
+        divisor_sum = 1
+        for y in range(2, math.ceil(math.sqrt(x))):
+            if x % y == 0:
+                divisor_sum += y + x // y
+        proper_divisor_sums[x] = divisor_sum
+
+    for x, y in proper_divisor_sums.items():
+        if 1 < y < len(proper_divisor_sums) and x == proper_divisor_sums[y] and x != y:
+            sum_of_amicable_numbers += x
+
+    print(sum_of_amicable_numbers)
+
+def compute():
+	# Compute sum of proper divisors for each number
+	divisorsum = [0] * 10000
+	for i in range(1, len(divisorsum)):
+		for j in range(i * 2, len(divisorsum), i):
+			divisorsum[j] += i
+	
+	# Find all amicable pairs within range
+	ans = 0
+	for i in range(1, len(divisorsum)):
+		j = divisorsum[i]
+		if j != i and j < len(divisorsum) and divisorsum[j] == i:
+			ans += i
+	return str(ans)
+
+euler()
 
 
